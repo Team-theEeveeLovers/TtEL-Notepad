b188d2fad40e9c1f3b9aba5a879c88b8f098707b
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-07 15:01:05 -0400

    Add global libraries

---
+++ TtEL-NotepadSDL3/globals.h
@@ -0,0 +1,10 @@
+#pragma once
+#if defined(WIN32) && !defined(UNIX)
+#define WINDOWS
+
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#elif defined(UNIX) && !defined(WIN32)
+#define POSIX
+#endif
