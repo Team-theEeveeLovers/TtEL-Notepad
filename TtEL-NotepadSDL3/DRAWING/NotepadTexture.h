6dfc1d9feed7ec70049fe6aa70777903bcb86496
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-07 14:56:16 -0400

    Start work on custom texture type

---
+++ TtEL-NotepadSDL3/DRAWING/NotepadTexture.h
@@ -0,0 +1,27 @@
+#ifndef NOPD_TXTR
+#define NOPD_TXTR
+#include <SDL.h>
+class NOTEPAD_Texture {
+public:
+	// Initalize
+	NOTEPAD_Texture();
+
+	SDL_Texture* tex;
+};
+
+NOTEPAD_Texture::NOTEPAD_Texture() {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
