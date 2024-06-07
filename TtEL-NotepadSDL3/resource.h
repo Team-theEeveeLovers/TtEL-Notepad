06176fdbc5c7c9bd61350fe58614a9a7363a20ed
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-07 15:02:11 -0400

    Add resource files for Windows

---
+++ TtEL-NotepadSDL3/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TtEL-SDL3_Notepad.rc
+//
+#define IDI_ICON1                       101
+#define IDC_ARROW                       102
+#define IDC_IBEAM                       103
+#define IDB_BITMAP1                     105
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
