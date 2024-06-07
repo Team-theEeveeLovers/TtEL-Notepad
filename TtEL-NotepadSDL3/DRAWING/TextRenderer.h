49de77f0c65ad88533a41a1ee83e88e51aab9269
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-07 15:05:02 -0400

    Add texture draw source control for text textures

--- TtEL-NotepadSDL3/DRAWING/TextRenderer.h
+++ TtEL-NotepadSDL3/DRAWING/TextRenderer.h
@@ -7,6 +7,7 @@ extern float scr_floatwid;
 extern float scr_floathei;
 extern SDL_Renderer* main_renderer;
 #include "VectorTypes.h"
+#include "../globals.h"
 
 class character {
 private:
@@ -16,29 +17,40 @@ private:
 	char magic2[4] = { 'C', 'h', 'a', 'r'};
 	// This is padding between the magic number and the letter contained by the variable, it also uses unused memory space
 	char padding[1] = { '\x20' };
+	// The unused space is caused by the letter variable
 public:
 	// This is the letter contained by the character
 	char letter[1] = "";
 	// This is the texture of the letter
-	SDL_Texture* letter_texture;
+	SDL_Texture* letter_texture = NULL;
 	// This is the x position of the character
 	float x = 0.f;
 	// This is the y position of the character
 	float y = 0.f;
 	// This is the width of the character
-	int w = 0;	
+	Uint8 w = 0;	
 	// This is the height of the character
-	int h = 0;
+	Uint8 h = 0;
+	// This is the x texture source position of the character
+	Uint8 sx = 0;
+	// This is the y texture source position of the character
+	Uint8 sy = 0;
+	// This is the width of the source rectangle of the character
+	Uint16 sw = 0;
+	// This is the height of the source rectangle of the character
+	Uint16 sh = 0;
 
 	void drawCharacter();
 	void freeCharacter();
-	void loadChar(char chrctr[1], SDL_Texture* textTexture, vector2_int size = vector2_int(48, 48));
+	void loadChar(char chrctr[1], SDL_Texture* textTexture, vector2_int size = vector2_int(48, 48), vector2_int source = vector2_int(0, 0));
 };
 
 void character::drawCharacter() {
 	SDL_FRect currentDraw = { x,y,static_cast<float>(w),static_cast<float>(h)};
-	SDL_FRect currentSource = { 0.f, 0.f,static_cast<float>(w),static_cast<float>(h) };
+	SDL_FRect currentSource = { static_cast<float>(sx), static_cast<float>(sy),static_cast<float>(w),static_cast<float>(h)};
 	SDL_RenderTexture(main_renderer, letter_texture, &currentSource, &currentDraw);
+	SDL_SetRenderDrawColor(main_renderer, 0xFF, 0x00, 0x00, 0xFF);
+	SDL_RenderRect(main_renderer, &currentDraw);
 	//SDL_free(&currentDraw);
 }
 
@@ -56,10 +68,12 @@ void character::freeCharacter() {
 	SDL_DestroyTexture(letter_texture);
 }
 
-void character::loadChar(char chrctr[1], SDL_Texture* textTexture, vector2_int size) {
+void character::loadChar(char chrctr[1], SDL_Texture* textTexture, vector2_int size, vector2_int source) {
 	letter[0] = chrctr[0];
 	letter_texture = textTexture;
 	w = size.x;
 	h = size.y;
+	sx = source.x;
+	sy = source.y;
 };
 #endif
