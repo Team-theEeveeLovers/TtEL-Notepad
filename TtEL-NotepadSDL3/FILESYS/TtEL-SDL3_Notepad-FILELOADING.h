11bbec23649d378e71da8bfe24c0837cd5b6cf4d
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-08 17:31:43 -0400

    Create "TtEL-SDL3_Notepad-FILELOADING.h"

---
+++ TtEL-NotepadSDL3/FILESYS/TtEL-SDL3_Notepad-FILELOADING.h
@@ -0,0 +1,8 @@
+#pragma once
+/**
+* @file TtEL-SDL3_Notepad-FILELOADING.h
+* @brief A header file of the TtEL SDL3 GUI Notepad, containing file loading functions
+* @author Team theEeveeLovers Sean
+* @date June 8th, 2024
+*/
+
