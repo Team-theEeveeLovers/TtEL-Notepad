5efdfffaacc24d7d8ec9d2a1ac2173feaa57fd98
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-08 15:36:28 -0400

    Add doxygen file descriptor

--- TtEL-NotepadSDL3/DRAWING/SDL_Surface-Tools.h
+++ TtEL-NotepadSDL3/DRAWING/SDL_Surface-Tools.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "../globals.h"
 #include <SDL.h>
+/**
+* @file SDL_Surface-Tools.h
+* @brief A header file of the TtEL SDL3 GUI Notepad, containing functions for dealing with SDL_Surface types.
+* @author Team theEeveeLovers Sean
+* @date June 8th, 2024
+*/
 
 Uint32 GetSurfacePixel32(Uint32 x, Uint32 y, SDL_Surface* surface) {
 	Uint32* pixels = static_cast<Uint32*>(surface->pixels);
