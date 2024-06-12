fc4e3d224cf45c1f652316ab8c9ca5c620eaa4e2
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-12 14:26:53 -0400

    Make Assert and Debug_Break global Can be called from any header

--- TtEL-NotepadSDL3/TtEL-SDL3_Notepad.hpp
+++ TtEL-NotepadSDL3/TtEL-SDL3_Notepad.hpp
@@ -6,11 +6,6 @@ using namespace std;
 #include <SDL3_image/SDL_image.h>
 
 
-#ifndef ASSERT
-#include <assert.h>
-#define ASSERT(_EXPR)            assert(_EXPR)                              
-#endif
-
 #include "TtEL-SDL3_Notepad-MATH.h"
 // Wrapper for mathext
 namespace ME = mathext;
@@ -34,22 +29,6 @@ extern float scr_floathei;
 #ifdef DRAW_DEBUG
 extern bool DRAW_DBG;
 #endif
-
-#ifndef DEBUG_BREAK
-#if defined (_MSC_VER)
-#define DEBUG_BREAK()    __debugbreak()
-#elif defined(__clang__)
-#define DEBUG_BREAK()    __builtin_debugtrap()
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-#define DEBUG_BREAK()    __asm__ volatile("int3;nop")
-#elif defined(__GNUC__) && defined(__thumb__)
-#define DEBUG_BREAK()    __asm__ volatile(".inst 0xde01")
-#elif defined(__GNUC__) && defined(__arm__) && !defined(__thumb__)
-#define DEBUG_BREAK()    __asm__ volatile(".inst 0xe7f001f0")
-#else
-#define DEBUG_BREAK()    ASSERT(0)    // It is expected that you define DEBUG_BREAK() into something that will break nicely in a debugger!
-#endif
-#endif // #ifndef DEBUG_BREAK
 
 extern SDL_Texture* textTextures[256];
 extern vector2_int textTextureSizeVectors[256];
