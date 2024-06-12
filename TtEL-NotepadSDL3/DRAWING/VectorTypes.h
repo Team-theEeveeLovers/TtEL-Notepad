2cf90b5abd517422a98ec6896cef360452ec9a1e
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-12 15:43:51 -0400

    Fix vector2_float class and add vector3 classes vector2_float accidentally stored integers instead of floats vector3 stores 3 values instead of vector2 only storing 2

--- TtEL-NotepadSDL3/DRAWING/VectorTypes.h
+++ TtEL-NotepadSDL3/DRAWING/VectorTypes.h
@@ -8,9 +8,20 @@ typedef struct vector2_int {
 	constexpr vector2_int(int _x, int _y)	: x(_x), y(_y) { }
 } vector2_int;
 typedef struct vector2_float {
-	int										x, y;
+	float									x, y;
 	constexpr vector2_float() : x(0.f), y(0.f) { }
 	constexpr vector2_float(float _x, float _y) : x(_x), y(_y) { }
 } vector2_float;
+
+typedef struct vector3_int {
+	int										x, y, z;
+	constexpr vector3_int() : x(0), y(0), z(0) { }
+	constexpr vector3_int(int _x, int _y, int _z) : x(_x), y(_y), z(_z) { }
+} vector3_int;
+typedef struct vector3_float {
+	float									x, y, z;
+	constexpr vector3_float() : x(0.f), y(0.f), z(0.f) { }
+	constexpr vector3_float(float _x, float _y, float _z) : x(_x), y(_y), z(_z) { }
+} vector3_float;
 
 #endif 
