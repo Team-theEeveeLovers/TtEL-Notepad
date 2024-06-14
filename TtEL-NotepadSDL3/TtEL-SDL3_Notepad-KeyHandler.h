6a5730efcac6aec7dfbabda00bf8ab6f9f7ef6d0
Sean O'Neal <seanoneal2013@outlook.com>
2024-06-14 19:38:53 -0400

    Add new header for offloading keyboard handling Will clean up code in main source file

---
+++ TtEL-NotepadSDL3/TtEL-SDL3_Notepad-KeyHandler.h
@@ -0,0 +1,8 @@
+#pragma once
+/**
+* @file TtEL-SDL3_Notepad-KeyHandler.h
+* @brief A header file of the TtEL SDL3 GUI Notepad, containing keyboard handling functions
+* @author Team theEeveeLovers Sean
+* @date June 14th, 2024
+*/
+#include <SDL.h>
