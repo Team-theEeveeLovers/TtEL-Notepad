2aff06cee839c16116078ab88f4f70a81e404ef1
Sean O'Neal <sdubs08@gmail.com>
2024-07-05 21:39:55 -0400

    [FIXED] Emulate newer API function with older API calls

--- TtEL-NotepadSDL3/TtEL-SDL3_Notepad.hpp
+++ TtEL-NotepadSDL3/TtEL-SDL3_Notepad.hpp
@@ -2,6 +2,33 @@
 #include <iostream>
 using namespace std;
 #include <SDL.h>
+// If the person is using an older version of SDL3
+#ifndef SDL_GetRendererName() // emulate the API function
+/**
+ * Get the name of a renderer.
+ *
+ * The returned string follows the SDL_GetStringRule.
+ * 
+ * @deprecated THIS IS NOT AN API CALL, IT IS A FUNCTIONAL EMULATION OF ONE FROM A NEWER VERSION.
+ * PLEASE UPDATE YOUR SDL3 VERSION.
+ *
+ * \param renderer the rendering context.
+ * \returns the name of the selected renderer, or NULL if the renderer is
+ *          invalid.
+ *
+ * \since This function is available since SDL 3.0.0.
+ *
+ * \sa SDL_CreateRenderer
+ * \sa SDL_CreateRendererWithProperties
+ */
+const char *SDL_GetRendererName(SDL_Renderer *renderer) {
+	SDL_RendererInfo renderer_info;
+	if (SDL_GetRendererInfo(renderer, &renderer_info) < 0)
+		return NULL;
+	else
+		return renderer_info.name;
+}
+#endif
 #include <SDL3_ttf/SDL_ttf.h>
 #include <SDL3_image/SDL_image.h>
 
